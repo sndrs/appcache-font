6fd1818d9d34c65791f7160b802da7d9916c41db
Alex Sanders <a@snd.rs>
2015-11-29 20:49:30 +0000

    fixes, hash etc

--- make-font-css.js
+++ make-font-css.js
@@ -4,21 +4,20 @@
 
 const fs = require('fs');
 const chalk = require('chalk');
-const cssFile = 'fonts.css';
+const cssFile = cssHash => `fonts.${cssHash}.css`;
 const mainfestFile = 'manifest.appcache';
 
 const fontFaceTemplate = (family, weight, style, data) => `@font-face {
     font-family: "${family}";
-    src: url(data:application/x-font-woff;base64,${data});
+    src: url("data:application/x-font-woff;base64,${data}");
     font-weight: ${weight};
-    font-style: "${style}";
+    font-style: ${style};
 }`;
 
 const manifestTemplate = (cssHash) => `CACHE MANIFEST
-# ${cssHash}
 
 CACHE:
-/${cssFile}
+/${cssFile(cssHash)}
 
 NETWORK:
 *`;
@@ -27,8 +26,8 @@ const sizeTemplate = (string) => `${chalk.blue(require('pretty-bytes')(require('
 
 // generate the CSS file
 const css = require('./typefaces.json').map(typeface => {
-    const css = typeface.fonts.map(font =>
-        fontFaceTemplate(
+    const css = typeface.fonts.map(font => {
+        const css = fontFaceTemplate(
             typeface.family,
             font.weight,
             font.style,
@@ -37,25 +36,35 @@ const css = require('./typefaces.json').map(typeface => {
                     console.log('errror reading font', err);
                 }
             })
-        )
-    ).join('\n');
+        );
+        console.log(`${typeface.family} (${font.weight}, ${font.style}): ${sizeTemplate(css)}`);
+        return css;
+    }).join('\n');
     console.log(`${typeface.family} (${typeface.fonts.length} variants): ${sizeTemplate(css)}`);
     return css;
 }).join('\n');
 
+const cssHash = require('crypto').createHash('md5').update(css).digest('hex');
+
 // save the CSS to disk
-fs.writeFile(cssFile, css, 'utf-8', err => {
+fs.writeFile(cssFile(cssHash), css, 'utf-8', err => {
     if (err) {
         console.log('error writing CSS', err);
     }
 });
 
 // generate the manifest
-fs.writeFile(mainfestFile, manifestTemplate(require('crypto').createHash('md5').update(css).digest('hex')), 'utf-8', err => {
+fs.writeFile(mainfestFile, manifestTemplate(cssHash), 'utf-8', err => {
     if (err) {
         console.log('error writing manifest', err);
     }
 });
+
+require("replace")({
+    regex: '<style data-fonts-URI="/fonts.*\.css">',
+    replacement: `<style data-fonts-URI="/fonts.${cssHash}.css">`,
+    paths: ["./index-localstorage.html"]
+  })
 
 console.log(`Total: ${sizeTemplate(css)}
 `)
